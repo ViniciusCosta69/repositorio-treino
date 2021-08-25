093b536c572c93a6d016ec3cadc816b2d6d73844
Vinícius Camilo Costa <88341837+ViniciusCosta69@users.noreply.github.com>
2021-08-25 12:26:24 -0300

    Add files via upload

---
+++ ambiente.js
@@ -0,0 +1,27 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo${c}`)
+    c++
+} Estrutura de repetição teste logico no inicio */
+
+var c = 1
+do{
+    console.log(`Passo${c}`)
+    c++
+} while(c<= 6)
+//Estrutura repetição no final
+
+
+
+
+
+/*
+console.log('Tudo bem)
+console.log('Tudo bem)
+console.log('Tudo bem)
+console.log('Tudo bem)
+console.log('Tudo bem)
+console.log('Tudo bem)
+console.log('Tudo bem)
+repetição desnecessaria tendo o mesmo resultado do outro*/
